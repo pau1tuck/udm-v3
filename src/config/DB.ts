d20fa3cd2175b5ba61de21d5ccbef5adc62c10be
Paul Tuck <paul.tuck@oxon.org>
2024-04-21 22:24:43 +0700

    chore: disable noDefaultExport

--- src/config/DB.ts
+++ src/config/DB.ts
@@ -1,12 +1,12 @@
-import { createClient } from '@libsql/client';
-import { drizzle } from 'drizzle-orm/libsql';
-import { migrate } from 'drizzle-orm/libsql/migrator';
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+import { migrate } from "drizzle-orm/libsql/migrator";
 
-import { Env } from './Env.mjs';
+import { Env } from "../libs/Env.mjs";
 
 const client = createClient({
-  url: Env.DATABASE_URL,
-  authToken: Env.DATABASE_AUTH_TOKEN,
+    url: Env.DATABASE_URL,
+    authToken: Env.DATABASE_AUTH_TOKEN,
 });
 
 export const db = drizzle(client);
@@ -14,6 +14,6 @@ export const db = drizzle(client);
 // Disable migrate function if using Edge runtime and use `npm run db:migrate` instead.
 // Only run migrate in development. Otherwise, migrate will also be run during the build which can cause errors.
 // Migrate during the build can cause errors due to the locked database when multiple migrations are running at the same time.
-if (process.env.NODE_ENV !== 'production') {
-  await migrate(db, { migrationsFolder: './migrations' });
+if (process.env.NODE_ENV !== "production") {
+    await migrate(db, { migrationsFolder: "./migrations" });
 }
