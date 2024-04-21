c4f6b0366283e1aaf9f672aeb9ef9d1b140d0f06
Paul Tuck <paul.tuck@oxon.org>
2024-04-21 22:43:07 +0700

    chore: clear home page

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -5,7 +5,7 @@ const localePrefix: LocalePrefix = "as-needed";
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
     name: "UDM | Underground Dance Music",
-    locales: ["en", "fr"],
+    locales: ["en"],
     defaultLocale: "en",
     localePrefix,
 };
